df4874a5d8509a37d00afe9669cbfa74640220c6
eralpkor <eralp.ny@gmail.com>
2019-07-26 14:23:52 -0400

    more classes stretch solution.

--- challenges/classes.js
+++ challenges/classes.js
@@ -23,24 +23,8 @@
     }
   }
 
-/* == Step 2: Volume Method ==
-  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
-  
-  Formula for cuboid volume: length * width * height
-*/
 
 
-/* == Step 3: Surface Area Method ==
-  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
-
-  Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
-*/
-
-
-/* == Step 4: Create a new object that uses CuboidMaker ==
-  Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
-  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
-*/
 
 let cuboid1 = new CuboidMaker({
   length: 4,
@@ -69,5 +53,17 @@ let cuboid2 = new CubeMaker({
   height: 3
 });
 
+let cuboid3 = new CubeMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
 console.log(`Cube maker stretch solution ${cuboid2.volume()}`);
 console.log(`Cube maker stretch solution ${cuboid2.surfaceArea()}`);
+
+console.log(`Cube maker stretch solution ${cuboid3.volume()}`);
+
+console.log(`Cube maker stretch solution ${cuboid3.surfaceArea()}`);
+
+
