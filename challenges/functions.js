8da668d8ec638e163ccdc5053bef223ba0573ee2
eralpkor <eralp.ny@gmail.com>
2019-07-26 11:14:59 -0400

    sone with functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume(par1, par2, callback) {
+  return callback(par1, par2);  
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,12 +16,22 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2) {
+  return num1 + num2;
+}
 
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +39,11 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// A function can access all variables defined inside the function
+// all functions have access to the scope above them, this is called JavaScript closure.
+// nestedFunction can access to `internal` because myFunction is the parent of nestedFunction. nestedFunction has outside scope from it's parent. Functions can
+// access outside scope but if there were a variable inside nestedFunction myFunction 
+// couldn't access to nestedFucntion's scope.
 
 const external = "I'm outside the function";
 
